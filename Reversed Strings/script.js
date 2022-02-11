a699767981dec268a94e8e96015151ef10848aea
Aleksandr Masko <amasko18@gmail.com>
2022-02-11 18:18:18 +0300

    commit

---
+++ Reversed Strings/script.js
@@ -0,0 +1,13 @@
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    return str.split('').reverse().join('')
+  }
+  reverseString("hello"); // olleh
+
+
+
+  //split - разбивает строку на символы
+  //reversed - переворачивает
+  //join - собирает строку из массива
