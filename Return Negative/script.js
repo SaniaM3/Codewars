8c7a32d93949e7b37b2931526676e63fd4166259
Aleksandr Masko <amasko18@gmail.com>
2022-02-12 20:13:06 +0300

    commit

--- Return Negative/script.js
+++ Return Negative/script.js
@@ -4,9 +4,9 @@
 // makeNegative(0.12); // return -0.12
 
 function makeNegative(num) {
-    return (num > 0) ? -num : (num < 0) ? +num : 0
+    return (num > 0) ? -num : (num < 0) ? +num : 0  //return num < 0 ? num : -num; можно еще сократить
   };
 
-//   условие ? выражение1 : выражение2 
+// условие ? выражение1 : выражение2 
 // Если num > 0, то возвращаем -num, иначе если num < 0,
 // возвращаем +num, иначе возвращаем 0
