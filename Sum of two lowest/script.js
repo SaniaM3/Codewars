ec00f3e22d908d629e20c852b3775cd1a04da1d7
Aleksandr Masko <amasko18@gmail.com>
2022-02-17 09:34:11 +0300

    commit

---
+++ Sum of two lowest/script.js
@@ -0,0 +1,24 @@
+//вернуть сумму первых двух наименьших чисел
+// Например, когда массив передается как [19, 5, 42, 2, 77], вывод должен быть 7.
+
+function sumTwoSmallestNumbers(numbers) {  
+    var ordered = numbers.sort(function(a,b){return a-b;});
+    var result=0;
+    
+    for(i=0; i<ordered.length; i++){
+      if(i==0){
+        result+=ordered[0];
+      }
+      if(i==1){
+        result+=ordered[1];
+      }
+    }
+    return result;
+  };
+
+  //версия короче
+
+  function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort((a,b) => a - b);
+    return numbers[0] + numbers[1];
+  };
