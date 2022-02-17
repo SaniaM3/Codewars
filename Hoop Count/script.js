44aaa83179767c30482fcbe3a9a027bf076406ff
Aleksandr Masko <amasko18@gmail.com>
2022-02-17 11:06:47 +0300

    commit

---
+++ Hoop Count/script.js
@@ -0,0 +1,6 @@
+//if else упрощенный.
+//если больше или равно 10, keep..., иначе great....
+
+function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
