118155371e5e9dfd97506015c8e32cd552353b58
Aleksandr Masko <amasko18@gmail.com>
2022-02-19 23:36:48 +0300

    commit

--- For Me/script.js
+++ For Me/script.js
@@ -5,3 +5,11 @@ function checkSign(num) {
   }
   
   checkSign(10);
+
+  //тернарное выражение 3 условий
+
+function checkSign(num) {
+  return (num == 0) ? "zero" : (num < 0) ? "negative" : "positive";
+}
+
+checkSign(10);
