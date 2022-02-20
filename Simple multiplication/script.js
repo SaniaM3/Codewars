6762fc2c5f841fcf0407121c86b9f7bc778dcadb
Aleksandr Masko <amasko18@gmail.com>
2022-02-20 22:42:39 +0300

    commit

---
+++ Simple multiplication/script.js
@@ -0,0 +1,12 @@
+// Это ката об умножении заданного числа на восемь, 
+// если оно четное, и на девять в противном случае.
+
+function simpleMultiplication(value){
+    
+    if(value%2===0 ){
+     return value *8
+    }
+    else{
+    return value* 9;
+    }    
+}
