265354e6132f27417190f3703972e219146bfecb
Aleksandr Masko <amasko18@gmail.com>
2022-02-21 22:06:30 +0300

    commit

--- Simple multiplication/script.js
+++ Simple multiplication/script.js
@@ -1,15 +1,13 @@
 // Это ката об умножении заданного числа на восемь, 
 // если оно четное, и на девять в противном случае.
 
-const completion = require("gulp-cli/lib/shared/completion");
-
 function simpleMultiplication(value){
     
     if(value%2===0 ){
-     return value *8
+     return value * 8
     }
     else{
-    return value* 9;
+    return value * 9;
     }    
 }
 
