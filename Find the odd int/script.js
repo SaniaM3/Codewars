1287f9c44387cd515802f8bd30d2cc43df9fa843
Aleksandr Masko <amasko18@gmail.com>
2022-02-22 19:51:08 +0300

    commit

---
+++ Find the odd int/script.js
@@ -0,0 +1,24 @@
+// Дан массив целых чисел, найдите то, 
+// которое встречается нечетное количество раз. 
+// Всегда будет только одно целое число, 
+// которое встречается нечетное количество раз.
+
+function findOdd(A) {
+    let count = 0;
+    let arr = A.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+          count++;
+        }
+      }
+      if (count % 2 !== 0) {
+        return arr[i];
+      }
+    }
+  }
+
+
+  // проще решение
+
+  const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
