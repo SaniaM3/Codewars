6a3dc225e352529d28cadc0e16beb6b405f4e12e
Aleksandr Masko <amasko18@gmail.com>
2022-03-07 20:14:41 +0300

    commit

---
+++ str end/script.js
@@ -0,0 +1,13 @@
+// Подтвердить окончание буквы
+
+function confirmEnding(str, target) {
+    var newStr = '';
+  
+    newStr = str.substring(str.length - target.length);
+  
+    return newStr === target;
+  
+  }
+  
+  confirmEnding("Bastian", "n");
+  .
